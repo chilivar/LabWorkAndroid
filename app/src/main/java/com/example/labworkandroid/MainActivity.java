56e2bbc441fc2ae29fa33e8b59fdd7abb3b16047
Maxim Plugov <107909771+chilivar@users.noreply.github.com>
2025-09-24 22:28:35 +0500

    Сделал лаб2

--- app/src/main/java/com/example/labworkandroid/MainActivity.java
+++ app/src/main/java/com/example/labworkandroid/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.labworkandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageBox;
+    private Button clearBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        imageBox = findViewById(R.id.imageBox);
+        clearBtn = findViewById(R.id.clearBtn);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.bracketRdBtn) {
+                    imageBox.setImageResource(R.drawable.bracket);
+                } else if (checkedId == R.id.routerRdBtn) {
+                    imageBox.setImageResource(R.drawable.router);
+                } else if (checkedId == R.id.usbFlashExodiaRdBtn) {
+                    imageBox.setImageResource(R.drawable.usb_flash_exodia);
+                } else if (checkedId == R.id.rackRdBtn) {
+                    imageBox.setImageResource(R.drawable.rack);
+                } else if (checkedId == R.id.basicMouseRdBtn) {
+                    imageBox.setImageResource(R.drawable.mouse);
+                } else if (checkedId == R.id.logitechModelRdBtn) {
+                    imageBox.setImageResource(R.drawable.logitech_mouse);
+                }
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageBox.setImageResource(R.drawable.basket);
+                radioGroup.clearCheck();
+            }
         });
     }
 }
